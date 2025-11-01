7712ddedb8ed77c89565aac6b6afb3fd799964f3
lamchiwing <60570944+lamchiwing@users.noreply.github.com>
2025-11-01 10:35:18 +0000

    Update PacksPage.tsx

--- apps/frontend/src/pages/PacksPage.tsx
+++ apps/frontend/src/pages/PacksPage.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { titleFromSlug, subjectZh, gradeZh, normalizeSlug } from "../data/titles";
-import { titleFromSlug, subjectZh, gradeZh, normalizeSlug } from "../data/titles";
 
 type Pack = {
   slug: string;
