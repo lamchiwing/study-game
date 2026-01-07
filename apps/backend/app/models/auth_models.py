115f8d5f6e7199baffef8822ebc4a33f1d692d7d
lamchiwing <60570944+lamchiwing@users.noreply.github.com>
2026-01-07 19:19:04 +0100

    Import User and LoginCode models in auth_models.py

--- apps/backend/app/models/auth_models.py
+++ apps/backend/app/models/auth_models.py
@@ -7,6 +7,7 @@ from sqlalchemy import String, Integer, Boolean, TIMESTAMP, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
+from app.models import User, LoginCode
 
 
 class User(Base):
